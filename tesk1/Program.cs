8463fba10f6a902b41ab6e422a19e6a8b9042deb
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-08 21:46:30 +0500

    add new commit

---
+++ tesk1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Пример 4-го вида из леции
+ string Method4(int count, string c)
+{
+    int i = 0;
+    string result = ""; //Пустую строку можно написать String.Empty
+
+    while (i < count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result;
+}
+
+string res =  Method4(10, "мама, я гей");
+Console.WriteLine(res);
