d35dfdfc21b9e32ffbb65ab13bd411f4b00b26c3
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-12 18:09:55 +0500

    add new commit

---
+++ for/Program.cs
@@ -0,0 +1,14 @@
+﻿//Пример 4-го вида из леции циклом for
+ string Method4(int count, string c)
+{
+    string result = ""; //Пустую строку можно написать String.Empty
+    
+    for( int i = 0; i < count; i++)
+    {
+        result = result + c;
+    }
+    return result;
+}
+
+string res =  Method4(10, "мама, я гей! ");
+Console.WriteLine(res);
